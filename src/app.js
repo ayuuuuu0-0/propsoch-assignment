fd9742217e37d6dfdbe735907732b8a49c84d945
Ayush Ranjan <ranaayush0725@gmail.com>
2026-09-19 11:50:59 +0530

    Simplified user identification to use x-user-id header per assignment spec

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/auth', require('./routes/auth.routes'));
 app.use('/api/v1/users', require('./routes/user.routes'));
 app.use('/api/v1/expenses', require('./routes/expense.routes'));
 app.use('/api/v1/balances', require('./routes/balance.routes'));
